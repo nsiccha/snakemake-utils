aa07170be3fa6de352b1d601b19bc144c24abb4b
Nikolas Siccha <nikolassiccha@gmail.com>
2023-10-24 08:12:51 +0300

    autocommit

---
+++ python/snakemake-utils/auto.py
@@ -0,0 +1,10 @@
+import inspect
+
+
+def identity(x): return x
+
+def auto_snakemake(snakemake, fn, post=identity):
+    rv = fn(snakemake.input, snakemake.output, **snakemake.wildcards)
+    if rv is None: return
+    write_json(snakemake.output[0], post(dict(**snakemake.wildcards) | rv))
+    return rv
