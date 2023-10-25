6dab34062e91bd1cacfbf25d2c6346bc1106cb21
Nikolas Siccha <nikolassiccha@gmail.com>
2023-10-25 20:04:21 +0300

    autocommit

--- example/scripts/test.py
+++ example/scripts/test.py
@@ -2,5 +2,6 @@ import snakemakeutils
 
 @snakemakeutils.auto(globals())
 def test(input, **kwargs):
+    if kwargs["seed"] % 2: raise ValueError("I fail if seed % 2!")
     yield kwargs      
          
