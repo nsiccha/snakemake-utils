c2f5579cd06f9e48f1a95270202faf84f8328a29
Nikolas Siccha <nikolas.siccha@aalto.fi>
2023-10-26 11:26:19 +0300

    autocommit

--- examples/dev/snakemake/common.smk
+++
@@ -1,4 +0,0 @@
-rule lint:
-    input: "snakefile", "snakemake/common.smk"
-    output: "output/lint.log"
-    shell: "snakemake --lint > {output} 2> {output} || echo 'Check linting output!'"
--- examples/pypi/scripts/test.py
+++
@@ -1,7 +0,0 @@
-import snakemakehelpers
-
-@snakemakehelpers.auto(globals())
-def test(input, **kwargs):
-    if int(kwargs["seed"]) % 2: raise ValueError("I fail if seed % 2!")
-    yield kwargs      
-         
